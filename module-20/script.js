19c96e27f7ee14f532216079e5dffbfe09340f94
Sam <ss11976@nyu.edu>
2023-12-13 17:18:09 -0800

    updating js

--- module-20/script.js
+++ module-20/script.js
@@ -1,6 +1,12 @@
 function myFunction() {
     var x = document.getElementById("menuButton").getAttribute("aria-expanded"); 
-    if (x == "true") {x = "false"} 
-    else {x = "true"}
+    if (x == "true") {
+        x = "false"
+        console.log("switch off")
+    } 
+    else {
+        x = "true"
+        console.log("switch on")
+    }
     document.getElementById("menuButton").setAttribute("aria-expanded", x);
 }
